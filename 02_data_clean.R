c3d6a19f9a3cb64fdfd21e9e934f8159b9e22b24
Victor Perry <Victor.Perry@walmartlabs.com>
2019-03-15 18:08:25 -0700

    progress

---
+++ 02_data_clean.R
@@ -0,0 +1,102 @@
+library(tidyverse)
+library(rvest)
+library(lubridate)
+
+spring18 <- readRDS("match_data/all_matches_2018spring.Rdata")
+spring18playoffs <- readRDS("match_data/all_matches_2018springplayoffs.Rdata")
+fall18 <- readRDS("match_data/all_matches_2018fall.Rdata")
+fall18playoffs <- readRDS("match_data/all_matches_2018fallplayoffs.Rdata")
+spring19 <- readRDS("match_data/all_matches_2019spring.Rdata")
+
+# With this, need functions from 01_data_scrape.Rmd
+#spring19 <- 
+#  spring19 %>% 
+#  bind_rows(week_urls_to_all_matches("https://www.sfpapool.org/stats/week/63/"))
+
+# Get combined file
+results <-
+  bind_rows(
+    spring18,
+    spring18playoffs,
+    fall18,
+    fall18playoffs,
+    spring19
+  )
+
+results_17_18 <-
+  bind_rows(
+    spring18,
+    spring18playoffs,
+    fall18,
+    fall18playoffs
+  )
+
+results_19 <-
+  spring19
+
+# Pull out forfeits and omitted playoff games from data frames, clean names
+remove_forfeits <- function(results_df) {
+  results_df %>% 
+    filter(is.na(forfeit)) %>% 
+    filter(!is.na(game_winner)) %>% 
+    filter(home != "Forfeited Game") %>% 
+    filter(away != "Forfeited Game") %>% 
+    mutate(
+      home = str_replace(home, "\\(", ""),
+      home = str_replace(home, "\\)", ""),
+      away = str_replace(away, "\\(", ""),
+      away = str_replace(away, "\\)", ""),
+      home = str_replace_all(home, '\\"', ""),
+      away = str_replace_all(away, '\\"', ""),
+      home = str_replace(home, "\\'HJ\\'", "HJ"),
+      away = str_replace(away, "\\'HJ\\'", "HJ"),
+      home = str_replace(home, "\\'Stkyrice\\'\\s", ""),
+      away = str_replace(away, "\\'Stkyrice\\'\\s", ""),
+      home_team = str_replace(home_team, "é", "e"),
+      away_team = str_replace(away_team, "é", "e"),
+      home_team = str_replace(home_team, "!", ""),
+      away_team = str_replace(away_team, "!", ""),
+      home = if_else(home == "Chris L", "Chris Logan", home),
+      away = if_else(away == "Chris L", "Chris Logan", away),
+      home = if_else(home == "Jerz", "Jerz Zuluaga", home),
+      away = if_else(away == "Jerz", "Jerz Zuluaga", away),
+      home_team = if_else(home_team == "The Black Willows", "Black Willows", home_team),
+      away_team = if_else(away_team == "The Black Willows", "Black Willows", away_team),
+      home_team = if_else(home_team == "Lucky Horseshoe", "Lucky Horseshoe Caballeros", home_team),
+      away_team = if_else(away_team == "Lucky Horseshoe", "Lucky Horseshoe Caballeros", away_team),
+      home_team = if_else(home_team == "Smoke and Rumors", "Smoke & Rumors", home_team),
+      away_team = if_else(away_team == "Smoke and Rumors", "Smoke & Rumors", away_team),
+      p1_start_rating = as.numeric(NA), 
+      p1_end_rating = as.numeric(NA),
+      p2_start_rating = as.numeric(NA), 
+      p2_end_rating = as.numeric(NA)
+    )
+}
+
+results_17_18_no_forfeits <- remove_forfeits(results_17_18)
+results_19_no_forfeits <- remove_forfeits(results_19)
+results_no_forfeits <- remove_forfeits(results)
+train <- results_17_18_no_forfeits
+test <- results_19_no_forfeits
+
+# Get optimal home advantage parameter
+# Remove week 1 since teams in the same bar play the same table
+actual_home_win_pct <- 
+  results_no_forfeits %>% 
+  filter(week_number > 1) %>% 
+  group_by(game_winner) %>% 
+  count() %>% 
+  group_by() %>% 
+  mutate(pct = n / sum(n)) %>% 
+  filter(game_winner == "home") %>% 
+  pull(pct)
+
+home_advantage <- log((1 - actual_home_win_pct) / actual_home_win_pct) / log(10) * -400
+home_advantage
+
+results_no_forfeits %>% 
+  filter(week_number > 1) %>% 
+  group_by(season, game_winner) %>% 
+  count() %>% 
+  group_by(season) %>% 
+  mutate(pct = round(n / sum(n), 2))
--- player_ratings/app.R
+++ player_ratings/app.R
@@ -1,9 +1,12 @@
 library(shiny)
+library(shinyBS)
 library(tidyverse)
 library(rvest)
 library(lubridate)
 
-team_matches_and_ratings_sliced <- readRDS("team_matches_and_ratings_sliced2019-03-15.Rdata")
+team_matches_and_ratings_sliced <- 
+  readRDS("team_matches_and_ratings_sliced2019-03-15.Rdata") %>% 
+  mutate(new_rating = round(new_rating))
 
 plot_teams <- function(teams_of_interest) {
   team_matches_and_ratings_sliced %>% 
@@ -13,37 +16,45 @@ plot_teams <- function(teams_of_interest) {
     geom_point(color = "gray60", size = 1.8, alpha = 0.6) +
     geom_line(aes(color = team, group = team), size = 1, data = . %>% filter(team %in% teams_of_interest)) +
     geom_point(aes(color = team, group = team), size = 2.2, data = . %>% filter(team %in% teams_of_interest)) +
+    geom_text(
+      aes(label = new_rating, color = team, x = match_date + 10), 
+      size = 6.5, 
+      data = . %>% filter(team %in% teams_of_interest) %>% group_by(team) %>% slice(n()),
+      show.legend = FALSE
+    ) +
     labs(
-      x = "Match Date", y = "ELO Rating",
+      x = "Match Date", 
+      y = "ELO Rating",
       title = "SFPA Team ELO Ratings, 2018-2019 Seasons"
     ) +
     theme(
       legend.title = element_blank(),
-      plot.title = element_text(hjust = 0.5, size = 20),
-      axis.title.y = element_text(size = 16),
+      plot.title = element_text(hjust = 0.5, size = 24),
+      axis.title.y = element_text(size = 20),
       axis.title.x = element_blank(),
-      axis.text = element_text(size = 14),
+      axis.text = element_text(size = 16),
       legend.position = "bottom",
-      legend.text = element_text(size = 14)
+      legend.text = element_text(size = 16)
     )
 }
 
-ui <- bootstrapPage(
-  selectInput(
-    inputId = "Teams", 
-    label = "Choose team(s) to highlight:",
-    choices = team_matches_and_ratings_sliced %>% select(team) %>% arrange(team) %>% distinct(),
-    multiple = TRUE
-  ),
-  plotOutput(
-    outputId = "plot",
-    width = "90%",
-    height = "600px"
+ui <- fluidPage(
+  fluidRow(
+    selectInput(
+      inputId = "Teams", 
+      label = "Choose team(s) to highlight:",
+      choices = team_matches_and_ratings_sliced %>% select(team) %>% arrange(team) %>% distinct(),
+      multiple = TRUE
+    ),
+    plotOutput(
+      outputId = "plot",
+      width = "90%",
+      height = "600px"
+    )
   )
 )
-?selectInput
 
-server <- function(input, output) {
+server <- function(input, output, session) {
   output$plot <- renderPlot({
     plot_teams(input$Teams)
   })
@@ -51,4 +62,3 @@ server <- function(input, output) {
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
